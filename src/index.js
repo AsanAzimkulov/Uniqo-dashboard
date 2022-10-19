d0e43db9c4fff3b3e10afae2ed3e336f7d07399b
Asan Azimkulov <azimkulov40@gmail.com>
2022-10-19 15:09:39 +0600

    правки

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import { install } from "resize-observer";
+
+install();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
