e2a13bb1aec6c5c04625d0ed8f654146af9570fa
Asan Azimkulov <azimkulov40@gmail.com>
2022-10-19 18:15:51 +0600

    правки

--- src/components/ScrollBar/index.jsx
+++ src/components/ScrollBar/index.jsx
@@ -1,7 +1,11 @@
+import { useRef } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars-2';
 import styles from './index.module.scss';
 
 export function CustomScrollbar({ maxHeight, ...props }) {
+  const target = useRef(null);
+
+  setTimeout(() => {target.current.dispatchEvent(new Event('click', {bubbles: true}))}, 1000)
 
   return (
     <Scrollbars
@@ -10,7 +14,13 @@ export function CustomScrollbar({ maxHeight, ...props }) {
       renderThumbHorizontal={(props) => <div {...props} className='thumb-horizontal' />}
       renderThumbVertical={(props) => <div {...props} className={styles.thumb} />}
       renderView={(props) => (
-        <div {...props} className={styles.container} style={{ maxHeight: maxHeight }} id="no-scrollbar"/>
+        <div
+          {...props}
+          className={styles.container}
+          style={{ maxHeight: maxHeight }}
+          id='no-scrollbar'
+          ref={target}
+        />
       )}>
       {props.children}
     </Scrollbars>
